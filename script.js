2bfa3b04b1d7046e5e123c7a2496c26a818a3ca9
Abd El Rahman Osama <aerosama793@gmail.com>
2021-06-02 12:45:35 +0200

    first page of the website and some images

---
+++ script.js
@@ -0,0 +1,9 @@
+let navButtons = document.getElementById("navButtons");
+
+function showMenu() {
+    navButtons.style.right = "0";
+}
+
+function closeMenu() {
+    navButtons.style.right = "-200px";
+}
